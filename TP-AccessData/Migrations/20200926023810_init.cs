68bda5613bfe946c0c0ca1d78ca88dce321236ca
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2020-09-26 14:11:58 -0300

    Create initial migrations from models created

---
+++ TP-AccessData/Migrations/20200926023810_init.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TP_AccessData.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turnos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdEspecialista = table.Column<int>(nullable: false),
+                    IdPaciente = table.Column<int>(nullable: false),
+                    IdConsultorio = table.Column<int>(nullable: false),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    HoraInicio = table.Column<DateTime>(nullable: false),
+                    HoraFin = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turnos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalendarioTurnos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiaId = table.Column<int>(nullable: true),
+                    HoraInicio = table.Column<DateTime>(nullable: false),
+                    HoraFin = table.Column<DateTime>(nullable: false),
+                    IdEspecialista = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarioTurnos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CalendarioTurnos_Dias_DiaId",
+                        column: x => x.DiaId,
+                        principalTable: "Dias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarioTurnos_DiaId",
+                table: "CalendarioTurnos",
+                column: "DiaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarioTurnos");
+
+            migrationBuilder.DropTable(
+                name: "Turnos");
+
+            migrationBuilder.DropTable(
+                name: "Dias");
+        }
+    }
+}
---
+++ TP-AccessData/Migrations/20200926024710_defaultDays.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TP_AccessData.Migrations
+{
+    public partial class defaultDays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Dias",
+                columns: new[] {"nombre"},
+                values: new object[,] {
+                    {"Lunes"},
+                    {"Martes"},
+                    {"Miercoles"},
+                    {"Jueves"},
+                    {"Viernes"},
+                    {"Sabado"},
+                    {"Domingo"}
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
