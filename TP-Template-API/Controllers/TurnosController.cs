12d9c39f4e85fad91e2bc07ed44eb35a5f07e733
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2020-09-26 14:16:40 -0300

    Created turnos controller and added POST /turnos endpoint

---
+++ TP-Template-API/Controllers/TurnosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TP_Application.Services;
+using TP_Domain.DTOs;
+
+namespace TP_Template_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TurnosController:ControllerBase
+    {
+        private readonly ITurnoService _service;
+        public TurnosController(ITurnoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public IActionResult Post(TurnoDto alumno)
+        {
+            try
+            {
+                return new JsonResult(_service.CreateTurno(alumno)) { StatusCode = 201 };
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
