6dd41603a0790dd286065012f53b9e27239c9868
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2020-09-26 14:10:00 -0300

    Added dto for turno

---
+++ TP-Domain/DTOs/TurnoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_Domain.DTOs
+{
+    public class TurnoDto
+    {
+        public int IdEspecialista { get; set; }
+        public int IdPaciente { get; set; }
+        public int IdConsultorio { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFin { get; set; }
+    }
+}
