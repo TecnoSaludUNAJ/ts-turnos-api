770486f4a3bf03a215e40ee2cefb8fdfd61d671d
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2020-09-26 14:13:09 -0300

    Added turno service and added method for create turno

---
+++ TP-Application/Services/TurnoService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TP_Domain.Commands;
+using TP_Domain.DTOs;
+using TP_Domain.Entities;
+
+namespace TP_Application.Services
+{
+    public interface ITurnoService
+    {
+        Turno CreateTurno(TurnoDto turno);
+    }
+    public class TurnoService : ITurnoService
+    {
+        private readonly IGenericRepository _repository;
+        public TurnoService(IGenericRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Turno CreateTurno(TurnoDto turno)
+        {
+            Turno entity = new Turno
+            {
+                IdEspecialista = turno.IdEspecialista,
+                IdPaciente = turno.IdPaciente,
+                IdConsultorio = turno.IdConsultorio,
+                Fecha = DateTime.Today,
+                HoraInicio = turno.HoraInicio,
+                HoraFin = turno.HoraFin
+            };
+
+            _repository.Add<Turno>(entity);
+            
+            return entity;
+        }
+    }
+}
